90ef48b87009c56c2ab0da661dc11b48200f1ca9
Bellvr <168459537+Bellvr@users.noreply.github.com>
2025-11-16 02:37:42 -0300

    me olvide guardar main :(

--- src/componentes/paginas/veterinario/miPerfil/Main.jsx
+++ src/componentes/paginas/veterinario/miPerfil/Main.jsx
@@ -34,6 +34,6 @@ export default function Main() {
 
     return (
         // 1. Corrección del nombre de la prop
-        <Formulario perfil={perfil} />  
+        <Formulario perfil={perfil} /> 
     );
 }
